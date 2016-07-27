0d83393061106613c4032bd010dbe280d5141388
Adriano Godoy <godoy.ccp@gmail.com>
2016-07-27 10:27:02 -0300

    Allows render options

--- app/helpers/infinite_helper.rb
+++ app/helpers/infinite_helper.rb
@@ -1,9 +1,12 @@
 module InfiniteHelper
-  def infinite_append(containerSelector, items)
-    html = "$('" + containerSelector + "').append('"+ j(render items) + "');"
+  def infinite_append(containerSelector, render_options)
+    collection = render_options
+    collection = render_options[:collection] unless render_options.is_a?(ActiveRecord::Relation)
 
-    if items.next_page
-      html += "$('.infinite-pagination').replaceWith('" + j(will_paginate(items, renderer: WillPaginateInfinite::InfinitePagination)) + "');"
+    html = "$('" + containerSelector + "').append('"+ j(render render_options) + "');"
+
+    if collection.next_page
+      html += "$('.infinite-pagination').replaceWith('" + j(will_paginate(collection, renderer: WillPaginateInfinite::InfinitePagination)) + "');"
     else
       html += "$(window).off('scroll');"
       html += "$('.infinite-pagination').remove();"
@@ -11,5 +14,4 @@ module InfiniteHelper
 
     html.html_safe
   end
-
 end

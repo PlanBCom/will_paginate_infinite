aa1a021b3fa42ee6839054900cc69682825ea0dd
MohamedAlaa <malaa83@gmail.com>
2018-09-21 02:38:10 +0200

    fixing the javascript to be compatible with jquery3

--- app/assets/javascripts/will_paginate_infinite.js
+++ app/assets/javascripts/will_paginate_infinite.js
@@ -1,11 +1,11 @@
 jQuery(function() {
-  if ($('.infinite-pagination').size() > 0) {
+  if ($('.infinite-pagination').length > 0) {
     $(window).on('scroll', function() {
       var more_posts_url = $('.infinite-pagination a.next_page').attr('href');
       var bottom_distance = 20;
 
       if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - bottom_distance) {
-        $('.infinite-pagination').html('Carregando...');
+        $('.infinite-pagination').html('<div class="infinite-loading">Loading...</div>');
         $.getScript(more_posts_url);
       }
     });
